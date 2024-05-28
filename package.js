42fa972b223137f129892cdfa7281616bfdb9ba4
jankapunkt <jkuester@uni-bremen.de>
2024-05-28 21:39:50 +0200

    build: resolve package versions for Meteor 3.0 compatibility

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'johanbrook:publication-collector',
-  version: '1.1.0',
+  version: '2.0.0-rc.0',
   summary: 'Test a Meteor publication by collecting its output.',
   documentation: 'README.md',
   git: 'https://github.com/johanbrook/meteor-publication-collector.git',
@@ -15,7 +15,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom(['1.3', '2.8', '3.0-rc.2']);
 
   api.use([
     'ecmascript',
@@ -33,7 +33,7 @@ Package.onTest(function(api) {
     'mongo',
     'random',
     'meteortesting:mocha',
-    'accounts-password@1.3.1',
+    'accounts-passwor',
     'underscore'
   ], 'server');
 
