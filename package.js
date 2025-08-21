f6cd8bf46d23815df32f5ea13502e49388431843
jankapunkt <jkuester@uni-bremen.de>
2025-08-21 12:17:15 +0200

    release: 2.0.0-rc.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: "communitypackages:publication-collector",
-  version: "2.0.0-rc.0",
+  version: "2.0.0-rc.1",
   summary: "Test a Meteor publication by collecting its output.",
   documentation: "README.md",
   git: "https://github.com/johanbrook/meteor-publication-collector.git",
